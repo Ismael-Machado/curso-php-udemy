b6f49f7d4bb3971496a13bbbdf6d8cd7cc742952
Ismael Machado <ismael.silva@sou.ufac.br>
2022-09-30 13:10:39 -0500

    tipos de dados - inteiros

--- index.php
+++ index.php
@@ -36,22 +36,17 @@
           <ul>
             <li><a href="exercicio.php?dir=basico&file=ola">Ola PHP</a></li>
             <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
-            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
-            <!--
-            <li><a href="#">Exercício B</a></li>
-            <li><a href="#">Exercício C</a></li>
-            -->
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li> 
           </ul>
         </div>
-        <!-- 
+         
         <div class="modulo vermelho">
-          <h3>Módulo 02</h3>
+          <h3>Módulo 02 - Tipos</h3>
           <ul>
-            <li><a href="#">Exercício A</a></li>
-            <li><a href="#">Exercício B</a></li>
-            <li><a href="#">Exercício C</a></li>
+            <li><a href="exercicio.php?dir=tipos&file=int">Inteiros</a></li>
           </ul>
         </div>
+        <!--
         <div class="modulo azul">
           <h3>Módulo 03</h3>
           <ul>
---
+++ tipos/int.php
@@ -0,0 +1,12 @@
+<div class="titulo">Tipo inteiro</div>
+
+<?php
+
+echo 11; 
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+
+echo PHP_INT_MAX;
