fc54cfebc75aacefce8f24c279ab0a71deda405f
Ismael Machado <ismael.silva@sou.ufac.br>
2022-10-05 16:28:03 -0500

    atribuicoes aula

--- index.php
+++ index.php
@@ -57,6 +57,8 @@
           <ul>
             <li><a href="exercicio.php?dir=variaveis&file=basico">Basico</a></li>
             <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
+            <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
           </ul>
         </div>
         <!--
