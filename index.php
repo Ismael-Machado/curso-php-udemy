252540ddacd37db65f563f6706d6c6447dcab2cf
Ismael Machado <ismael.silva@sou.ufac.br>
2023-04-14 11:49:48 -0500

    updates

--- index.php
+++ index.php
@@ -43,6 +43,10 @@
             <li><a href="exercicio.php?dir=classes_objetos&file=static">Membros Estáticos</a></li>
             <li><a href="exercicio.php?dir=classes_objetos&file=interface">Interface</a></li>
             <li><a href="exercicio.php?dir=classes_objetos&file=abstract">Classe Abstrata</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=desafio_erros">Desafio Erros</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&file=final">Modificador Final</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&fileca=traits_01">Traits #01</a></li>
+            <li><a href="exercicio.php?dir=classes_objetos&fileca=traits_02">Traits #02</a></li>
            
           </ul>
         </div>
